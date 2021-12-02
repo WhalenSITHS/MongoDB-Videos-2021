9e2603e749973df6c659ef398e3195cc2c522ace
mr. whalen <michael.whalen@sitechhs.com>
2021-12-02 08:43:04 -0500

    sdfg

--- Routes/index.js
+++ Routes/index.js
@@ -2,11 +2,19 @@ const express = require("express");
 const router = new express.Router(); //instantiate express router
 
 router.get("/", (req, res) => {
+  const teacher = { name: "Whalen", age: 31, tenure: true };
   try {
-    return res.send("We're Live");
+    res.send(req.query);
   } catch (error) {
     console.log(error);
   }
 });
 
+router.get("/teacher/:name", async (req, res) => {
+  try {
+    res.json(req.params.name);
+  } catch (error) {
+    console.log(error);
+  }
+});
 module.exports = router;
